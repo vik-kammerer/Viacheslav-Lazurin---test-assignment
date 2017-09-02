f62fe4367a9a842ab14ca08120d72a6d168fb70a
vik-kammerer <vik.kammerer@gmail.com>
2017-09-02 10:15:40 +0300

    Sign Up Cest created

---
+++ tests/acceptance/signUpCest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class signUpCest
+{
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
