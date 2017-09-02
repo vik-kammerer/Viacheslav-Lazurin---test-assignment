a934af31076059e505675d85820d725d5916597f
vik-kammerer <vik.kammerer@gmail.com>
2017-09-02 10:24:29 +0300

    Locators were included into Sign Up page object

--- tests/_support/Page/signUpPage.php
+++ tests/_support/Page/signUpPage.php
@@ -4,8 +4,31 @@ namespace Page;
 class signUpPage
 {
     // include url of current page
-    public static $URL = '';
 
+
+    
+//    Locators
+    public static $signUpURL = '/customer/account/create/';
+    public static $firstName = ['css' => 'firstname'];
+    public static $lastName = ['css' => '#lastname'];
+    public static $email = ['css' => '#email_address'];
+    public static $password = ['css' => '#password'];
+    public static $passConfirm = ['css' > '#password-confirmation'];
+    public static $createUser = ['css' => '.action.submit.primary'];
+    
+//    Test data
+    public static $testerPass = "1234567ABC_abc";
+    public static $testerName = "testerName";
+    public static $testerSurname = "testerLastName";
+  
+    
+
+    
+    
+
+    
+    
+    
     /**
      * Declare UI map for this page here. CSS or XPath allowed.
      * public static $usernameField = '#username';
