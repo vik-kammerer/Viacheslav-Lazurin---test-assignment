7f0ff8c680a48c95051f1d585cc8e753f5e36281
vik-kammerer <vik.kammerer@gmail.com>
2017-09-02 10:25:31 +0300

    Syntax errors fixed

--- tests/_support/Page/signUpPage.php
+++ tests/_support/Page/signUpPage.php
@@ -9,11 +9,11 @@ class signUpPage
     
 //    Locators
     public static $signUpURL = '/customer/account/create/';
-    public static $firstName = ['css' => 'firstname'];
+    public static $firstName = ['css' => '#firstname'];
     public static $lastName = ['css' => '#lastname'];
     public static $email = ['css' => '#email_address'];
     public static $password = ['css' => '#password'];
-    public static $passConfirm = ['css' > '#password-confirmation'];
+    public static $passConfirm = ['css' => '#password-confirmation'];
     public static $createUser = ['css' => '.action.submit.primary'];
     
 //    Test data
