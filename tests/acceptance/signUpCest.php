b59d19cce67a077cd06ba1380deb4b085dfc868e
vik-kammerer <vik.kammerer@gmail.com>
2017-09-02 10:39:39 +0300

    Sign up scenario has been done

--- tests/acceptance/signUpCest.php
+++ tests/acceptance/signUpCest.php
@@ -1,18 +1,40 @@
 <?php
 
+use \Page\signUpPage as signUpPage;
+
+
 
 class signUpCest
+
 {
-    public function _before(AcceptanceTester $I)
-    {
-    }
 
-    public function _after(AcceptanceTester $I)
-    {
-    }
+        public function _before(\AcceptanceTester $I)
+        {
+        }
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
-    {
-    }
+        public function _after(\AcceptanceTester $I)
+        {
+            
+        }
+                    
+        public function CreateUser (\AcceptanceTester $I)
+        {
+            $I->wantTo('Create a user with unique email');
+            $I->amOnPage(signUpPage::$signUpURL);
+            $I->wait('2'); //wait untill all JS scipts are loaded
+            $I->fill(signUpPage::$firstName, signUpPage::$testerName);
+            $I->fill(signUpPage::$lastName, signUpPage::$testerSurname);
+            $I->fill(signUpPage::$email, self::$uniqueEmail = signUpPage::getRandomEmail());
+            $I->fill(signUpPage::$password, signUpPage::$testerPas);
+            $I->fill(signUpPage::$passConfirm, signUpPage::$testerPass);
+            $I->scrollTo(signUpPage::$createUser);
+            $I->click(signUpPage::$createUser);
+            
+
+        }
+
+
+        
+
+                
 }
