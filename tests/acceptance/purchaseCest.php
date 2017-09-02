db979e0250a1f2c8504aabf4e9b3e61337733e10
vik-kammerer <vik.kammerer@gmail.com>
2017-09-02 11:18:43 +0300

    Add simple test added

--- tests/acceptance/purchaseCest.php
+++ tests/acceptance/purchaseCest.php
@@ -14,8 +14,16 @@ class purchaseCest
     }
 
     // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function purchaseSimple(AcceptanceTester $I)
     {
+        $I->wantTo('Add smple prodct into cart');
+        $I->amOnPage(PDP::$SimpleURL);
+        $I->seeElementInDOM(PDP::$productTitle);
+        $I->seeElementInDOM(PDP::$priceBox);
+        $I>fillField(PDP::$qtyBox, '10');
+        $I->click(PDP::$addingButton);
+        $I->waitForText('10', PDP::$counter);
+
         
     }
 }
